893ecf56a1d2cbe235ac85dec114a2207030fb5b
Thomas Le Floch <t.lefloch53gmail.com>
2020-02-12 14:25:05 +0100

    feat: added environnement checking at the start of the server

---
+++ src/config.js
@@ -0,0 +1,16 @@
+const expectedEnvVariables = [
+  { name: 'PORT', default: '3000' },
+];
+
+const getEnvDefaultValue = (entries, varName) => {
+  return entries.find(env => env.name === varName).default;
+};
+
+const config = {
+  port: process.env.PORT || getEnvDefaultValue(expectedEnvVariables, 'PORT'),
+};
+
+module.exports = {
+  expectedEnvVariables,
+  config
+}
---
+++ src/env-check.js
@@ -0,0 +1,30 @@
+/**
+ * Will print missing env variables, and default value if they have one
+ * Will throw an error if a missing env variable has no default value
+ * @param {[{name: string, default?: string }]} expectedEnv list of all the variable to check
+ * eg {name: PORT, default: '3000'} 
+ */
+const checkMissingEnvVariables = (expectedEnv) => {
+  const undefinedEnvVariables = expectedEnv.filter(env => {
+    return !process.env[env.name];
+  });
+
+  console.log();
+  let requiredEnvVarMissing = false;
+  console.log('Missing environnement variables:');
+  undefinedEnvVariables.forEach(env => {
+    if (env.default) {
+      console.log(`${env.name} \tusing default value ${env.default}`);
+    } else {
+      requiredEnvVarMissing = true;
+      console.error(`${env.name} \t no default value, please define it`);
+    }
+  });
+  console.log();
+
+  if (requiredEnvVarMissing) throw { message: 'Missing environnement variables, see above output' };
+};
+
+module.exports = {
+  checkMissingEnvVariables
+}
