faea7519d45f764f94ed248e70d45415995710eb
Thomas Le Floch <t.lefloch53gmail.com>
2020-02-12 15:56:45 +0100

    fix: table names and constraints

--- database/tables.sql
+++ database/tables.sql
@@ -1,28 +1,28 @@
-DROP TABLE achat;
+DROP TABLE purchase;
 DROP TABLE product;
 DROP TABLE client;
 
 CREATE TABLE client (
   id_client UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  name varchar,
-  created_at timestamp with time zone default now()
+  name VARCHAR(100),
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT now()
 );
 
 CREATE TABLE product (
   id_product UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  name varchar,
-  price float,
-  tax float,
-  created_at timestamp with time zone default now()
+  name VARCHAR(100) NOT NULL,
+  price FLOAT NOT NULL,
+  tax FLOAT NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT now()
 );
 
-CREATE TABLE achat (
-  id_client UUID,
-  id_product UUID,
-  created_at timestamp with time zone default now(),
-  PRIMARY KEY (id_client, id_product)
+CREATE TABLE purchase (
+  id_purchase UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  id_client UUID NOT NULL,
+  id_product UUID NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT now()
 );
 
-ALTER TABLE achat ADD FOREIGN KEY (id_client) REFERENCES client (id_client);
+ALTER TABLE purchase ADD FOREIGN KEY (id_client) REFERENCES client (id_client);
 
-ALTER TABLE achat ADD FOREIGN KEY (id_product) REFERENCES product (id_product);
+ALTER TABLE purchase ADD FOREIGN KEY (id_product) REFERENCES product (id_product);
